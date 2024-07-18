ae1cd80e714bc515387d375a0799e8b388ba90ad
Sanketydv1 <sanketkumar1919@gmail.com>
2024-07-19 01:39:36 +0530

    add new projet Real estate

---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./header.css";
+import { nav } from "../../data/Data";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [navList, setNavList] = useState(false);
+
+  return (
+    <>
+      <header>
+        <div className="container flex">
+          <Link to="/">
+            <img src="./images/logo.png" alt="" />
+          </Link>
+          <div className="nav">
+            <ul className={navList ? "small" : "flex"}>
+              {nav.map((list, index) => (
+                <li key={index}>
+                  <Link to={list.path}>{list.text}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="button flex">
+            <Link>call now +91 8892992761</Link>
+          </div>
+
+          <div className="toggle">
+            <button onClick={() => setNavList(!navList)}>
+              {navList ? (
+                <i className="fa fa-times"></i>
+              ) : (
+                <i className="fa fa-bars"></i>
+              )}
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/data/Data.js
@@ -0,0 +1,283 @@
+export const nav = [
+  {
+    text: "home",
+    path: "/",
+  },
+  {
+    text: "about",
+    path: "/about",
+  },
+  {
+    text: "services",
+    path: "/services",
+  },
+  {
+    text: "blog",
+    path: "/blog",
+  },
+  {
+    text: "contact",
+    path: "/contact",
+  },
+];
+export const featured = [
+  {
+    cover: "../images/hero/h1.png",
+    name: "Family House",
+    total: "122 Property",
+  },
+  {
+    cover: "../images/hero/h2.png",
+    name: "House & Villa",
+    total: "155 Property",
+  },
+  {
+    cover: "../images/hero/h3.png",
+    name: "Apartment",
+    total: "300 Property",
+  },
+  {
+    cover: "../images/hero/h4.png",
+    name: "Office & Studio",
+    total: "80 Property",
+  },
+  {
+    cover: "../images/hero/h6.png",
+    name: "Villa & Condo",
+    total: "80 Property",
+  },
+];
+export const list = [
+  {
+    id: 1,
+    cover: "../images/list/p-1.png",
+    name: "Red Carpet Real Estate",
+    location: "New Delhi, India",
+    category: "For Rent",
+    price: "$3,700",
+    type: "Apartment",
+  },
+  {
+    id: 2,
+    cover: "../images/list/p-2.png",
+    name: "The Real Estate Corner",
+    location: "NewYork",
+    category: "For Sale",
+    price: "$9,750",
+    type: "Condos",
+  },
+  {
+    id: 3,
+    cover: "../images/list/p-7.png",
+    name: "The Real Estate Corner",
+    location: "USA",
+    category: "For Rent",
+    price: "$5,860",
+    type: "Offices",
+  },
+  {
+    id: 4,
+    cover: "../images/list/p-4.png",
+    name: "The Real Estate Corner",
+    location: "Liverpool, London",
+    category: "For Sale",
+    price: "$7,540",
+    type: "Homes & Villas",
+  },
+  {
+    id: 5,
+    cover: "../images/list/p-5.png",
+    name: "The Real Estate Corner",
+    location: " Montreal, Canada",
+    category: "For Rent",
+    price: "$4,850",
+    type: "Commercial",
+  },
+  {
+    id: 6,
+    cover: "../images/list/p-6.png",
+    name: "The Real Estate Corner",
+    location: "India",
+    category: "For Sale",
+    price: "$2,742",
+    type: "Apartment",
+  },
+];
+export const awards = [
+  {
+    icon: <i class="fa-solid fa-trophy"></i>,
+    num: "32 M	",
+    name: "indian Marketer of the Year",
+  },
+  {
+    icon: <i class="fa-solid fa-briefcase"></i>,
+    num: "43 M",
+    name: "Mimo X11 Award",
+  },
+  {
+    icon: <i class="fa-solid fa-lightbulb"></i>,
+    num: "51 M",
+    name: "Australian UGC Award",
+  },
+  {
+    icon: <i class="fa-solid fa-heart"></i>,
+    num: "42 M",
+    name: "IITCA Green Award",
+  },
+];
+export const location = [
+  {
+    id: 1,
+    name: "New Delhi",
+    Villas: "12 Villas",
+    Apartments: "10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-1.png",
+  },
+  {
+    id: 2,
+    name: "Mumbai",
+    Villas: "12 Villas",
+    Apartments: "10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-2.png",
+  },
+  {
+    id: 3,
+    name: "Liverpool, London",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-3.png",
+  },
+  {
+    id: 4,
+    name: "NewYork, United States",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-4.png",
+  },
+  {
+    id: 5,
+    name: "Montreal, Canada",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-5.png",
+  },
+  {
+    id: 6,
+    name: "Bengaluru",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-6.png",
+  },
+];
+export const team = [
+  {
+    list: "50",
+    cover: "../images/customer/team-1.jpg",
+    address: "Liverpool, Canada",
+    name: "Sargam Singh",
+    icon: [
+      <i class="fa-brands fa-facebook-f"></i>,
+      <i class="fa-brands fa-linkedin"></i>,
+      <i class="fa-brands fa-twitter"></i>,
+      <i class="fa-brands fa-instagram"></i>,
+    ],
+  },
+  {
+    list: "70",
+    cover: "../images/customer/team-2.jpg",
+    address: "Montreal, Canada",
+    name: "Harijeet",
+    icon: [
+      <i class="fa-brands fa-facebook-f"></i>,
+      <i class="fa-brands fa-linkedin"></i>,
+      <i class="fa-brands fa-twitter"></i>,
+      <i class="fa-brands fa-instagram"></i>,
+    ],
+  },
+  {
+    list: "80",
+    cover: "../images/customer/team-3.jpg",
+    address: "Denever, USA",
+    name: "Sargam Singh",
+    icon: [
+      <i class="fa-brands fa-facebook-f"></i>,
+      <i class="fa-brands fa-linkedin"></i>,
+      <i class="fa-brands fa-twitter"></i>,
+      <i class="fa-brands fa-instagram"></i>,
+    ],
+  },
+  {
+    list: "51",
+    cover: "../images/customer/team-4.jpg",
+    address: "2272 Briarwood Drive",
+    name: "Harijeet",
+    icon: [
+      <i class="fa-brands fa-facebook-f"></i>,
+      <i class="fa-brands fa-linkedin"></i>,
+      <i class="fa-brands fa-twitter"></i>,
+      <i class="fa-brands fa-instagram"></i>,
+    ],
+  },
+  {
+    list: "42",
+    cover: "../images/customer/team-5.jpg",
+    address: "bengaluru, India",
+    name: "Anvar lal",
+    icon: [
+      <i class="fa-brands fa-facebook-f"></i>,
+      <i class="fa-brands fa-linkedin"></i>,
+      <i class="fa-brands fa-twitter"></i>,
+      <i class="fa-brands fa-instagram"></i>,
+    ],
+  },
+  {
+    list: "38",
+    cover: "../images/customer/team-5.jpg",
+    address: "Mumbai,India",
+    name: "Mohan Singh",
+    icon: [
+      <i class="fa-brands fa-facebook-f"></i>,
+      <i class="fa-brands fa-linkedin"></i>,
+      <i class="fa-brands fa-twitter"></i>,
+      <i class="fa-brands fa-instagram"></i>,
+    ],
+  },
+];
+export const footer = [
+  {
+    title: "LAYOUTS",
+    text: [
+      { list: "Home Page" },
+      { list: "About Page" },
+      { list: "Service Page" },
+      { list: "Property Page" },
+      { list: "Single Blog" },
+    ],
+  },
+  {
+    title: "ALL SECTIONS",
+    text: [
+      { list: "Headers" },
+      { list: "Features" },
+      { list: "Attractive" },
+      { list: "Testimonials" },
+      { list: "Videos" },
+    ],
+  },
+  {
+    title: "COMPANY",
+    text: [
+      { list: "About" },
+      { list: "Blog" },
+      { list: "Pricing" },
+      { list: "Affiliate" },
+      { list: "Login" },
+    ],
+  },
+];
